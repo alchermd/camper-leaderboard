f8d78715cc19962fc9b05cc819e823d0a103dedb
alchermd <johnalcherdoloiras@gmail.com>
2017-07-28 17:01:14 +0800

    Create dummy data form DisplayComponents to render.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import Last30 from './Last30'
 import AllTime from './AllTime'
+import DisplayCampers from './DisplayCampers'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      campers: [
+        {"username":"SkyC0der","img":"https://avatars2.githubusercontent.com/u/24684319?v=3","alltime":2027,"recent":354,"lastUpdate":"2017-07-27T09:07:55.970Z"},
+        {"username":"sjames1958gm","img":"https://avatars2.githubusercontent.com/u/4639625?v=3","alltime":7731,"recent":311,"lastUpdate":"2017-07-14T06:41:32.735Z"},
+        {"username":"Chrono79","img":"https://avatars0.githubusercontent.com/u/9571508?v=3","alltime":4129,"recent":306,"lastUpdate":"2017-06-28T11:24:49.080Z"},
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="container">
@@ -17,6 +29,8 @@ class App extends React.Component {
               <th><AllTime /></th>
             </tr>
           </thead>
+          
+          <DisplayCampers campers={this.state.campers} />
         </table>
       </div>
     )
