9b728ae93f9aa881f1187070e69f11bb28bf3df4
alchermd <johnalcherdoloiras@gmail.com>
2017-07-28 16:44:29 +0800

    Create base App framework.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import React from 'react'
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello, World!</h1>
+      <div className="container">
+        <h1 className="text-center">Camper Leaderboard</h1>
+
+        <table className="table table-striped table-bordered table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th><Last30 /></th>
+              <th><AllTime /></th>
+            </tr>
+          </thead>
+        </table>
       </div>
     )
   }
